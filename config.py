6e53849592349da39ef8db2d34d688bf533207ae
Arpan Chavda <arpanchavdaeng@gmail.com>
2014-03-30 22:27:00 +0530

    config

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 # -*- coding:utf-8 -*-
 """KindleEar的配置文件，开头两个配置项是必须修改的。"""
 
-SRC_EMAIL = "akindleear@gmail.com"  #邮件的发件人地址
-DOMAIN = "https://kindleear.appspot.com" #你的域名，用于归档链接
+SRC_EMAIL = "arpanchavdaeng@gmail.com"  #邮件的发件人地址
+DOMAIN = " http://calibre-box-21728.apse1.nitrousbox.com:3000 " #你的域名，用于归档链接
 
 TIMEZONE = 8  #默认时区
 
