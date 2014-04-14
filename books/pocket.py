d2e6286a0b11c3aa381aa331de7fcac650d79f24
Arpan Chavda <arpanchavdaeng@gmail.com>
2014-04-14 18:26:40 +0530

    pocket

---
+++ books/pocket.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from base import BaseFeedBook
+
+def getBook():
+    return pocket
+
+class pocket(BaseFeedBook):
+    title                 = u'My Pocket Feeds'
+    description           = u' Pocket ebook'
+    language              = 'en'
+    feed_encoding         = "utf-8"
+    page_encoding         = "utf-8"
+    #mastheadfile          = "mh_lifeweek.gif"
+    #coverfile             = "cv_lifeweek.jpg"
+    #oldest_article        = 7
+    
+    #deliver_days          = ['Saturday']
+    
+    feeds = [
+            (u'Current Feeds', 'http://getpocket.com/users/arpanchavdaeng/feed/read'),
+           ]
