d4f8a4432b70b634440363235a7d8013389c3823
Arpan Chavda <arpanchavdaeng@gmail.com>
2014-04-17 08:00:29 +0530

    frontline fix

--- books/frontline.py
+++ books/frontline.py
@@ -15,12 +15,13 @@ class frontline(BaseFeedBook):
     #coverfile             = "cv_lifeweek.jpg"
     oldest_article        = 7
     #max_articles_per_feed = 7
-    deliver_days          = ['Sunday','Tuesday','Thursday','Saturday']
+    #deliver_days          = ['Sunday','Tuesday','Thursday','Saturday']
+    fulltext_by_readability = True
     keep_image = True
     feeds = [
             (u'Recent Articles', 'http://www.frontline.in/?service=rss'),
            ]
-    def fetcharticle(self, url, decoder):
+    #def fetcharticle(self, url, decoder):
         #每个URL都增加一个后缀full=y，如果有分页则自动获取全部分页
-        url += '?css=print'
-        return BaseFeedBook.fetcharticle(self,url,decoder)
+        #url += '?css=print'
+        #return BaseFeedBook.fetcharticle(self,url,decoder)
