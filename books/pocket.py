040437195fde8df9436538ad3bcdcc1b19e10523
Arpan Chavda <arpanchavdaeng@gmail.com>
2014-04-26 10:35:26 +0530

    pocket fix

--- books/pocket.py
+++ books/pocket.py
@@ -15,6 +15,13 @@ class pocket(BaseFeedBook):
     #coverfile             = "cv_lifeweek.jpg"
     oldest_article        = 100
     #deliver_days          = ['Saturday']
+    fulltext_by_readability = False
+    
+    #如果为True则使用instapaper服务先清理网页，否则直接连URL下载网页内容
+    #instapaper的服务很赞，能将一个乱七八糟的网页转换成只有正文内容的网页
+    #但是缺点就是不太稳定，经常连接超时，建议设置为False
+    #这样你需要自己编程清理网页，建议使用下面的keep_only_tags[]工具
+    fulltext_by_instapaper = False
     feeds = [
             (u'Current Feeds', 'http://getpocket.com/users/arpanchavdaeng/feed/read'),
            ]
